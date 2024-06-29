a191957a6e7bfb1e5722e75d73d20abba5e859a4
LuisferRojas14 <luisfernandorojas@unicesar.edu.co>
2024-06-28 20:29:53 -0500

    feat: Add MovieScreen component with custom SliverAppBar and MovieDetails widget

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -7,10 +7,7 @@ class MovieScreen extends ConsumerStatefulWidget {
   static const name = 'movie-screen';
   final String movieId;
 
-  const MovieScreen({
-    super.key, 
-    required this.movieId
-    });
+  const MovieScreen({super.key, required this.movieId});
 
   @override
   MovieScreenState createState() => MovieScreenState();
@@ -19,17 +16,15 @@ class MovieScreen extends ConsumerStatefulWidget {
 class MovieScreenState extends ConsumerState<MovieScreen> {
   @override
   void initState() {
-    super.initState(
-    );
+    super.initState();
     ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
-
   }
 
   @override
   Widget build(BuildContext context) {
     final Movie? movie = ref.watch(movieInfoProvider)[widget.movieId];
 
-    if(movie == null){
+    if (movie == null) {
       return const Scaffold(
         body: Center(
           child: CircularProgressIndicator(strokeWidth: 2),
@@ -37,12 +32,100 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
       );
     }
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Movie Screen'),
-      ),
-      body: Center(
-        child: Text('Movie Screen ${widget.movieId}'),
+      body: CustomScrollView(
+        physics: const ClampingScrollPhysics(),
+        slivers: [
+          _CustomSliverAppBar(movie: movie),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) => _MovieDatails(movie: movie),
+              childCount: 1,
+            ),
+          )
+        ],
       ),
     );
   }
 }
+
+
+class _CustomSliverAppBar extends StatelessWidget {
+  final Movie movie;
+
+  const _CustomSliverAppBar({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return SliverAppBar(
+      backgroundColor: Colors.black,
+      expandedHeight: size.height * 0.7,
+      foregroundColor: Colors.white,
+      flexibleSpace: FlexibleSpaceBar(
+          titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          title: Text(
+            movie.title,
+            style: const TextStyle(fontSize: 20),
+            textAlign: TextAlign.start,
+          ),
+          background: Stack(
+            children: [
+              SizedBox.expand(
+                child: Image.network(
+                  movie.backdropPath,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox.expand(
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          stops: [0.5, 0.9],
+                          colors: [Colors.transparent, Colors.black87])),
+                ),
+              ),
+              const SizedBox.expand(
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          stops: [0.0, 0.3],
+                          colors: [Colors.black87, Colors.transparent])),
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+}
+
+class _MovieDatails extends StatelessWidget {
+  final Movie movie;
+
+  const _MovieDatails({required this.movie});
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(
+                  movie.posterPath,
+                  width:size.width * 0.3 ,
+                ))
+          ],
+        ),
+      ),
+    ]);
+  }
+}
+
+
+
